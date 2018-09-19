2e61304f8b71ea2206b80df196630e3a8f9e3934
Ana Nicole Massaneiro <ana.nicole02@gmail.com>
2018-09-19 00:07:10 -0300

    init da servletContext

--- JavaWEBII/src/java/com/ufpr/tads/web2/servlets/StartupServlet.java
+++ JavaWEBII/src/java/com/ufpr/tads/web2/servlets/StartupServlet.java
@@ -12,6 +12,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.*;
+
 
 /**
  *
@@ -19,16 +21,16 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet(name = "StartupServlet", urlPatterns = {"/StartupServlet"})
 public class StartupServlet extends HttpServlet {
+ServletConfig configuracao ;
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+
+  public void init(ServletConfig config)throws ServletException {
+
+    this.configuracao = config;
+
+    super.init(config);
+
+  }
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
